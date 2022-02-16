8e2be48f1ce8c13ccccb73b0ebb4db3443aba177
sugikazu75 <64698144+sugikazu75@users.noreply.github.com>
2022-02-17 00:10:12 +0900

    add servo control to stairing

--- craft_robot/sketch/main/main.ino
+++ craft_robot/sketch/main/main.ino
@@ -1,10 +1,9 @@
 #include <PS4Controller.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
+#include <ESP32Servo.h>
 
 const int power_pin = 2;
 const int ds4_connect_pin = 4;
+const int stairing_servo_pin = 17;
 const int IN_PLUS_left = 14;
 const int IN_MINUS_left = 27;
 const int IN_PLUS_right = 26;
@@ -44,13 +43,9 @@ void getPS4buttons(void);
 
 void settirespeed(void);
 
-void mpu6050setup(void);
-
-void getmpu6050(void);
-
 l298n_pwd left(EN_left, IN_PLUS_left, IN_MINUS_left);
 l298n_pwd right(EN_right, IN_PLUS_right, IN_MINUS_right);
-Adafruit_MPU6050 mpu;
+Servo stairing_servo;
 
 int L2=0, R2=0, CrossButton=0, L3X=0, L3Y=0, left_cmd=0, right_cmd=0;
 
@@ -75,87 +70,16 @@ void setup(){
   Serial.println("ds4 connected!");
   digitalWrite(4, HIGH);
 
-  // ===========mpu6050 setup=======
-  mpu6050setup();
+  // ========stairing servo===========
+  stairing_servo.attach(stairing_servo_pin);
 }
 
 void loop(){
   getPS4buttons();
   settirespeed();
+  stairing();
   delay(10);
-  getmpu6050();
-  delay(10);
-}
 
-void mpu6050setup(){
-  Serial.println("Adafruit MPU6050 test!");
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  Serial.println("MPU6050 Found!");
-  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-
-  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
 }
 
 void getPS4buttons(void){
@@ -169,8 +93,8 @@ void getPS4buttons(void){
     else CrossButton = 0;
     if(PS4.LStickX()) L3X = PS4.LStickX();
     else L3X = 0;
-    if(PS4.LStickY()) L3Y = PS4.LStickY();
-    else L3Y = 0;
+    // if(PS4.LStickY()) L3Y = PS4.LStickY();
+    // else L3Y = 0;
     if(PS4.Circle()) digitalWrite(power_pin, LOW);
     else digitalWrite(power_pin, HIGH);
   }
@@ -178,42 +102,19 @@ void getPS4buttons(void){
 }
 
 void settirespeed(void){
-  left_cmd = int(150 * float(R2)/255.0 + 100 * float(L3X)/128.0);
-  right_cmd = int(150 * float(R2)/255.0 - 100 * float(L3X)/128.0);
+  left_cmd = int(R2);
+  right_cmd = int(R2);
   if(CrossButton){
     left_cmd *= -1;
     right_cmd *= -1;
   }
   left.set_speed(left_cmd);
   right.set_speed(right_cmd);
-  Serial.println(left.SPEED);
-  Serial.println(right.SPEED);
-
+  // Serial.println(left.SPEED);
+  // Serial.println(right.SPEED);
 }
 
-void getmpu6050(){
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
-
-  Serial.println("");
+void stairing(void){
+  int stairing_servo_cmd = int(-25.0 * L3X / 128.0 + 105.0);
+  stairing_servo.write(stairing_servo_cmd);    
 }
